2d78c2227d9971ac0d22727460dcad2baaa9d8fe
Isaaq-Khader <ikhader@intrinsicschools.org>
2021-04-03 22:16:46 -0500

    Add code to display rap lyric

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function getLyric() {
-    
+    // from list of raps, randomly choose a rap lyric
+    const raps = ['Ur mum lol', 'git gud nerd'];
+    const rap = raps[Math.floor(Math.random() * raps.length)];
+
+    // retrieve the rap-container from the HTML and display it there
+    const rapContainer = document.getElementById('rap-container');
+    factContainer.innerText = rap;
 }
